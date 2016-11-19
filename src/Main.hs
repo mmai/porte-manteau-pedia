c950fb84fe92c55888c2f861aba912ccdeb9abcd
Henri Bourcereau <henri.bourcereau@gmail.com>
2016-11-19 18:57:08 +0100

    recursive random page fetch

--- src/Main.hs
+++ src/Main.hs
@@ -12,6 +12,7 @@ import qualified Data.ByteString.Char8 as C8
 
 import qualified Data.ByteString as B
 import Data.Char
+import Data.List
 import Text.HTML.TagSoup
 -- import Text.HTML.TagSoup hiding (parseTags, renderTags)
 -- import Text.HTML.TagSoup.Fast
@@ -19,6 +20,7 @@ import Text.HTML.TagSoup
 
 type Url = String
 
+data Page = Page { topic::String, text:: [String]} deriving (Show)
 -- data Page = Page { topic::ByteString, text:: [ByteString]} deriving (Show)
 
 randomUrl :: Url
@@ -27,31 +29,42 @@ randomUrl = "https://en.wikipedia.org/wiki/Special:Random"
 makeWikipediaUrl :: String -> Url
 makeWikipediaUrl = ( "https://en.wikipedia.org/wiki/" ++ )
 
-fetchPage :: Url -> IO String
--- fetchPage :: Url -> IO B.ByteString
--- fetchPage :: Url -> IO Page
-fetchPage url = do
+fetchPageCandidate :: Url -> IO Page
+fetchPageCandidate url = do
   r <- get url
-  let src = C8.unpack . BL.toStrict $ r ^. responseBody
-  let pageTopic = fromH1 $ parseTags src
-  return pageTopic
-  -- return $ Page "pageTopic" []
+  let tags = parseTags . C8.unpack . BL.toStrict $ r ^. responseBody
+  let pageTopic = fromH1 tags
+  -- let pageContent = fromParagraphs tags
+  return $ Page pageTopic []
 
--- fromH1 :: [Tag B.ByteString] -> B.ByteString
+fetchPage :: Url -> IO Page
+fetchPage url = do
+  putStrLn "Fetching page..."
+  page <- fetchPageCandidate url
+  -- skip lists
+  if (url == randomUrl && (
+    "List " `isPrefixOf` (topic page) ||
+    "Lists " `isPrefixOf` (topic page)
+    ))
+  then do
+    putStrLn "was a page list, fetch another"
+    (fetchPage url)
+  else (return page)
+
 fromH1 :: [Tag String] -> String
 fromH1 =  innerText . take 2 . dropWhile (~/= "<h1>")
 
-combinePages :: [String] -> String
--- combinePages :: [B.ByteString] -> B.ByteString
-combinePages [first, second] = concat [ first, "\n\n===========\n\n",  second ]
--- combinePages :: [Page] -> BL.ByteString
--- combinePages [first, second] = BL.concat [topic first, "\n\n===========\n\n", topic second ]
+-- fromParagraphs :: [Tag String] -> [String]
+-- fromParagraphs = []
+
+combinePages :: [Page] -> String
+combinePages [first, second] = concat ["\n", topic first, "n\n===========\n\n", topic second, "\n"]
 
 main :: IO ()
 main = do
   args <- getArgs
   pages <- mapM fetchPage $ parseArgs args
-  print $ combinePages pages
+  putStr $ combinePages pages
 
 parseArgs :: [String] -> [Url]
 parseArgs (arg1:(arg2:_)) = [makeWikipediaUrl arg1, makeWikipediaUrl arg2]
