cc75b84dabc7607cb7b25c07f4f580bd2a7cf481
Henri Bourcereau <henri.bourcereau@gmail.com>
2016-11-20 19:29:31 +0100

    functional clone of factMaker.py

---
+++ README.md
@@ -0,0 +1,6 @@
+# porte-manteau-pedia
+
+A simple script that takes two Wikipedia pages from user input or at random, and then merges them to create new facts.
+
+This is a Haskell clone of the python script https://github.com/mouse-reeve/fact-join 
+
--- src/Main.hs
+++ src/Main.hs
@@ -1,27 +1,29 @@
--- {-# LANGUAGE OverloadedStrings #-}
--- {-# OPTIONS_GHC -Wall #-}
-
 module Main where
 
 import System.Environment
 
 import Network.Wreq
-import Control.Lens
+import Control.Lens ((^.))
 import qualified Data.ByteString.Lazy as BL
 import qualified Data.ByteString.Char8 as C8
 
-import qualified Data.ByteString as B
 import Data.Char
 import Data.List
-import Text.HTML.TagSoup
--- import Text.HTML.TagSoup hiding (parseTags, renderTags)
--- import Text.HTML.TagSoup.Fast
 
+import Text.XML.HXT.Core
+import Text.HandsomeSoup
+import Text.Regex.TDFA ((=~))
+import Text.Regex
+
+import NLP.POS
+import NLP.Tokenize.String
 
 type Url = String
 
-data Page = Page { topic::String, text:: [String]} deriving (Show)
--- data Page = Page { topic::ByteString, text:: [ByteString]} deriving (Show)
+data Page = Page { topic::String, text::[String]} deriving (Show)
+
+replaceReg :: String -> String -> String -> String
+replaceReg regex replacement = flip (subRegex (mkRegex regex)) replacement
 
 randomUrl :: Url
 randomUrl = "https://en.wikipedia.org/wiki/Special:Random"
@@ -32,39 +34,83 @@ makeWikipediaUrl = ( "https://en.wikipedia.org/wiki/" ++ )
 fetchPageCandidate :: Url -> IO Page
 fetchPageCandidate url = do
   r <- get url
-  let tags = parseTags . C8.unpack . BL.toStrict $ r ^. responseBody
-  let pageTopic = fromH1 tags
-  -- let pageContent = fromParagraphs tags
-  return $ Page pageTopic []
+  let doc = readString [withParseHTML yes, withWarnings no] . C8.unpack . BL.toStrict $ r ^. responseBody
+  topics <- runX $ doc >>> css "h1" /> getText
+  paragraphs <- runX $ doc //> hasAttrValue "id" (== "mw-content-text") /> hasName "p" //> getText
+  if (url == randomUrl && length topics == 0 )
+  then do
+    putStrLn "No H1, fetch another"
+    fetchPageCandidate url
+  else
+    return (Page (head topics) paragraphs)
 
 fetchPage :: Url -> IO Page
 fetchPage url = do
   putStrLn "Fetching page..."
   page <- fetchPageCandidate url
-  -- skip lists
   if (url == randomUrl && (
-    "List " `isPrefixOf` (topic page) ||
-    "Lists " `isPrefixOf` (topic page)
-    ))
+       -- skip lists
+       ( "List " `isPrefixOf` (topic page) || "Lists " `isPrefixOf` (topic page) ) ||
+       -- skip disambiguation pages
+       length (text page) == 0 || (head (text page) =~ "[can|may] refer to" )
+     ))
   then do
     putStrLn "was a page list, fetch another"
     (fetchPage url)
-  else (return page)
+  else (return $ cleanSentences page)
 
-fromH1 :: [Tag String] -> String
-fromH1 =  innerText . take 2 . dropWhile (~/= "<h1>")
+cleanSentences :: Page -> Page
+cleanSentences (Page topic paragraphs) =
+  let content = concat $ intersperse " " paragraphs
+      cleanedContent =
+        replaceReg "\\[[0-9].?\\]" "" $
+        replaceReg "[[:space:]]+" " " $
+        replaceReg "\\(.*\\)" "" content
+      sentences = splitRegex (mkRegex "\\. ") cleanedContent
+   in Page topic sentences
 
--- fromParagraphs :: [Tag String] -> [String]
--- fromParagraphs = []
+-- tagPage :: PosTagger Tag -> Page -> [[String]]
+tagPage tagger page = filter hasVerb taggedSentences
+  where
+    taggedSentences = fmap tagSentence (text page)
 
-combinePages :: [Page] -> String
-combinePages [first, second] = concat ["\n", topic first, "n\n===========\n\n", topic second, "\n"]
+    tagSentence :: String -> [String]
+    tagSentence s = fmap (tagStr tagger) (tokenize s)
+
+    hasVerb :: [String] -> Bool
+    hasVerb = any ("/VB" `isInfixOf`)
+
+mergeSentences :: ([String],[String]) -> String
+mergeSentences (primary, secondary) = tagsToSentence $ (fst $ splitVerb primary) ++ (snd $ splitVerb secondary)
+  where
+    splitVerb :: [String] -> ([String], [String])
+    splitVerb tags = (untilVerb, afterVerb) where
+      (begin, end) = break ("/VB" `isInfixOf`) tags
+      untilVerb = begin ++ [head end]
+      afterVerb = tail end
+
+    tagsToSentence :: [String] -> String
+    tagsToSentence tags = cleanSentence . concat . (intersperse " ") $ fmap tagToWord tags
+
+    tagToWord :: String -> String
+    tagToWord tag = reverse word where
+      '/':word = snd $ span ( /= '/') (reverse tag)
+
+    cleanSentence :: String -> String
+    cleanSentence =
+      replaceReg " \\." "." .
+      replaceReg " \\," "."
 
 main :: IO ()
 main = do
   args <- getArgs
-  pages <- mapM fetchPage $ parseArgs args
-  putStr $ combinePages pages
+  [page1, page2] <- mapM fetchPage $ parseArgs args
+  tagger <- defaultTagger
+  let facts = fmap mergeSentences $ zip (tagPage tagger page1) (tagPage tagger page2)
+  putStrLn "\n================================="
+  putStrLn $ topic page1 ++ " + " ++ topic page2
+  putStrLn "=================================\n"
+  mapM_ putStrLn facts
 
 parseArgs :: [String] -> [Url]
 parseArgs (arg1:(arg2:_)) = [makeWikipediaUrl arg1, makeWikipediaUrl arg2]
